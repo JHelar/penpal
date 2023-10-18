b7d1aedb63a54bebb4a185721504b05ace4db678
John Larsson <john.helarsson@gmail.com>
2023-10-18 15:21:18 +0200

    fix: add auth routes, fix server fetch

---
+++ apps/web/src/routes/letters/+page.server.ts
@@ -0,0 +1,37 @@
+import { array, coerce, date, object, parse, string, uuid } from "valibot";
+
+const DateSchema = coerce(date(), (input) => new Date(input as string));
+
+const LetterSchema = object({
+  id: string([uuid()]),
+  message: string(),
+  to_user_id: string([uuid()]),
+  by_user_id: string([uuid()]),
+  created_at: DateSchema,
+  updated_at: DateSchema,
+});
+
+const LettersSchema = array(LetterSchema);
+
+/** @type {import('./$types').PageLoad} */
+export async function load({ fetch }) {
+  const lettersResponse = await fetch(
+    `${import.meta.env.VITE_SERVER_API_URL}/me/letter`,
+    {
+      headers: {
+        Authorization: `Bearer 66e3d037-d0c3-48af-b11c-b046b5c2ab63`,
+      },
+    },
+  );
+
+  if (!lettersResponse.ok) {
+    return {
+      letters: [],
+    };
+  }
+
+  const letters = parse(LettersSchema, await lettersResponse.json());
+  return {
+    letters,
+  };
+}
